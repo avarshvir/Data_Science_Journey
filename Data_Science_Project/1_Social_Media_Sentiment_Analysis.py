b49abd9f3ca0101146eb0d6d76e79fe52139cf3b
Arshvir <avarshvir@gmail.com>
2024-08-18 17:36:27 +0530

    commit

---
+++ Data_Science_Project/1_Social_Media_Sentiment_Analysis.py
@@ -0,0 +1,23 @@
+"""
+Sentiment Analysis is a process of extracting emotions or sentiments from a raw text using
+Natural Language Processing (NLP) and Computational Linguistics. People express their opinions
+and feelings on various social media platforms such as Twitter, Facebook, Instagram, etc.
+Social Media Sentiment Analysis can help organizations or marketers to understand how people
+perceive or feel about their brand, products, services, etc.
+"""
+# Import Required Libraries
+import re
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import string
+#import nltk
+#from wordcloud import WordCloud
+
+#%matplotlib inline
+
+# Read the dataset
+df = pd.read_csv('https://raw.githubusercontent.com/dD2405/Twitter_Sentiment_Analysis/master/train.csv')
+
+print(df.head())
